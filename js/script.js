3896d5210074458c475e3b475ed20691640c3db3
apexa11 <apexachoxi7@gmail.com>
2017-07-14 15:16:41 -0400

    update

--- js/script.js
+++ js/script.js
@@ -23,13 +23,13 @@ function loadData() {
 
     // AJAX request for NYtimes
 
-    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/aticlesearch.json?q="+cityStr+'&sort=newest&api-key=e620b09505834c0dbb674725514fc906'
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+cityStr+'&sort=newest&api-key=e620b09505834c0dbb674725514fc906'
     $.getJSON(nytimesUrl , function(data){
         $nytHeaderElem.text('New York Times Article ABout' + cityStr);
 
         articles = data.response.docs;
         for (var i = 0;i < articles.length;i++){
-            var article = article[i];
+            var article = articles[i];
             $nytElem.append('<li class = "article">' +
                     '<a href =" '+ article.web_url+' ">'+article.headline.main+
                         '</a>'
