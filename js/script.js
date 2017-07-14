820ec7517199622884804db4da94ff602922f62d
apexa11 <apexachoxi7@gmail.com>
2017-07-14 17:36:17 -0400

    fixes

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = streetStr +',' + cityStr;
 
-    $greeting.text ('So,you want to live at ' + address + '?');
+    $greeting.text ('So, You Want to live at ' + address + '!!');
 
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+ address +'';
     $body.append('<img class = "bgimg" src = " '+ streetviewUrl +' ">');
@@ -41,7 +41,27 @@ function loadData() {
     }).error(function(e){
         $nytHeaderElem.text('New York Times Article could not be loadded');
     });
-return false;
+
+    // AJAX request for Wikipedia
+
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    $.ajax({
+        url:wikiUrl,
+        dataType :"jsonp",
+        success : function(response){
+            var articleList = response[1];
+
+            for  (var i =0 ; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = "http://en.wikipedia.org/wiki/" + articleStr;
+                $wikiElem.append('<li> <a href = "'+ url + ' ">' + articleStr + '</a></li>');
+            };
+        }
+
+
+    });
+    return false;
 };
 
 $('#form-container').submit(loadData);
