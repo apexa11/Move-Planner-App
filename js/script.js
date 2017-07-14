cbf7a15456031df4b1dbfe3b278b298a34df53ee
apexa11 <apexachoxi7@gmail.com>
2017-07-14 14:18:22 -0400

    update

--- js/script.js
+++ js/script.js
@@ -18,18 +18,28 @@ function loadData() {
 
     $greeting.text ('So,you want to live at ' + address + '?');
 
-    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+ address +'';
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+ address +'';
     $body.append('<img class = "bgimg" src = " '+ streetviewUrl +' ">');
 
-    // AJAX call for NYtimes
-    $.getJSON(url , function(data){
+    // AJAX request for NYtimes
+
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/aticlesearch.json?q="+cityStr+'&sort=newest&api-key=e620b09505834c0dbb674725514fc906'
+    $.getJSON(nytimesUrl , function(data){
+        $nytHeaderElem.text('New York Times Article ABout' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0;i < articles.length;i++){
+            var article = article[i];
+            $nytElem.append('<li class = "article">' +
+                    '<a href =" '+ article.web_url+' ">'+article.headline.main+
+                        '</a>'
+                    +'<p>'+article.snippet + '</p>'+
+                 '</li>');
+
+        };
 
     })
-
-
-
-
-    return false;
+return false;
 };
 
 $('#form-container').submit(loadData);
